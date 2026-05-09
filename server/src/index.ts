328bff7e714305bdbabbe1358042ed457683d5d1
dangph12 <dangphhe181217@fpt.edu.vn>
2026-05-09 11:00:26 +0700

    feat: apply validator middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,27 @@
+import { ApiResponse } from '@app/shared/api-response';
+import { loginRequestSchema } from '@app/shared/dto/auth';
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 
+import { errorHandler } from './error-handler';
+import { validator } from './validate';
+
 const app = new Hono();
 
-app.get('/', c => {
-  return c.json({ message: 'Hello, Hono!' });
+app.onError(errorHandler);
+
+app.get('/health', c => {
+  return c.json({ status: 'ok' });
+});
+
+app.post('/login', validator('json', loginRequestSchema), c => {
+  const validated = c.req.valid('json');
+  // validated is automatically typed as LoginRequest
+  return c.json({ token: '...' });
+});
+
+app.notFound(c => {
+  return c.json(ApiResponse.failed('Not Found'), 404);
 });
 
 serve(
@@ -13,6 +30,6 @@ serve(
     port: 5000
   },
   info => {
-    console.log(`Server is running on http://localhost:${info.port}`);
+    console.warn(`Server is running on http://localhost:${info.port}`);
   }
 );
---
+++ server/src/validate.ts
@@ -0,0 +1,23 @@
+import { ApiResponse } from '@app/shared/api-response';
+import { zValidator } from '@hono/zod-validator';
+import type { ValidationTargets } from 'hono';
+import { z } from 'zod';
+import { fromZodError } from 'zod-validation-error';
+
+export const validator = <
+  T extends z.ZodSchema,
+  Target extends keyof ValidationTargets
+>(
+  target: Target,
+  schema: T
+) =>
+  zValidator(target, schema, (result, c) => {
+    if (!result.success) {
+      const readableError = fromZodError(result.error, {
+        prefix: null,
+        issueSeparator: '\\n'
+      });
+
+      return c.json(ApiResponse.failed(readableError.message), 400);
+    }
+  });
