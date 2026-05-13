8fbb21557bbe9121044f8fc4d055c4ab8ea03149
dangph12 <dangphhe181217@fpt.edu.vn>
2026-05-13 09:22:06 +0700

    feat: create dish model

---
+++ server/src/database/models/dish.ts
@@ -0,0 +1,62 @@
+import mongoose, { InferSchemaType, Schema } from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+
+const dishSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    imageUrl: {
+      type: String
+    },
+    servingSize: {
+      value: {
+        type: Number,
+        required: true
+      },
+      unit: {
+        type: String,
+        required: true
+      }
+    },
+    cookingTimeMinute: {
+      type: Number,
+      required: true
+    },
+    ingredients: [
+      {
+        ingredientId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Ingredient',
+          required: true
+        },
+        quantity: [
+          {
+            value: {
+              type: Number,
+              required: true
+            },
+            unit: {
+              type: String,
+              required: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  {
+    timestamps: true
+  }
+);
+
+dishSchema.plugin(mongoosePaginate);
+
+export type Dish = InferSchemaType<typeof dishSchema>;
+
+export const DishModel = mongoose.model<Dish>('Dish', dishSchema);
